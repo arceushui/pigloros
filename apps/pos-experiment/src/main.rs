daed117cc59152737b93e4f1821041c341764e42
jinhui.pang <jinhui.pang@motional.com>
2026-08-19 15:40:08 +0800

    fix(experiment): remove unused coverage_attribute feature from minimal stub

--- apps/pos-experiment/src/main.rs
+++ apps/pos-experiment/src/main.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-
 fn main() -> std::process::ExitCode {
     eprintln!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     std::process::ExitCode::SUCCESS
